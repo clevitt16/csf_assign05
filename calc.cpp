82df8ca604be47bfe8675af8136f8f5692ee8e40
Casey Levitt <69866213+clevitt16@users.noreply.github.com>
2021-04-08 22:26:59 -0400

    began calc functions

---
+++ calc.cpp
@@ -0,0 +1,109 @@
+#include "calc.h"
+#include <string>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <sstream>
+#include <bool>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::map;
+using std::vector;
+using std::stringstream;
+
+
+struct Calc {
+private:
+    map<string, int> variables;
+
+
+public:
+    // public member functions
+    Calc();
+    ~Calc();
+
+    int evalExpr(const std::string &expr, int &result) {
+		vector<string> tokens = tokenize(expr);
+		if (tokens.size() == 1) {
+			if (isInt(tokens[0])) {
+				
+			}
+		}
+		for (vector::iterator it = tokens.begin(); it != tokens.end(); it++) {
+			
+
+		}
+	}
+
+private:
+    // private member functions
+	vector<string> tokenize(const string &expr) {
+    	vector<string> vec;
+    	stringstream s(expr);
+    	string tok;
+    	while (s >> tok) {
+        	vec.push_back(tok);
+    	}
+    	return vec;
+	}
+
+	int isOp (const string &expr) {
+		char c = string[0];
+		switch(c) {
+			case '+':
+			case '-': 
+			case '*':
+			case '/':
+				return c;
+				break;
+			default:
+				return 0;
+		}
+		return 0;
+	}
+
+	
+	bool isInt (const string &expr) {
+		stringstream ss;
+		ss << expr;
+		int num;
+		ss >> num;
+		if (ss.fail()) {
+			return false;
+		}
+		return false;
+	}
+
+
+	bool isVarName (const string &expr) {
+		for (string::iterator it = expr.begin(); it != expr.end(); it++) {
+			char c = *it;
+			if (!(c >= 'A' && c <= 'Z') && !(c >= 'a' && c <= 'z')) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+};
+
+
+
+
+
+// for printing: cout << s.length() << endl
+
+
+extern "C" struct Calc *calc_create(void) {
+    return new Calc();
+}
+
+extern "C" void calc_destroy(struct Calc *calc) {
+    delete calc;
+}
+
+extern "C" int calc_eval(struct Calc *calc, const char *expr, int *result) {
+    return calc->evalExpr(expr, *result);
+}
