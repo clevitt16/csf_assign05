61fe02ee0dd0335cd3586138b765f4b33c5e80f7
Trisha Karani <trisha.karani@gmail.com>
2021-04-12 20:18:07 -0400

    fixed operations

--- calc.cpp
+++ calc.cpp
@@ -18,7 +18,6 @@ struct Calc {
 private:
     map<string, int> variables;
 
-
 public:
     // public member functions
     Calc();
@@ -41,23 +40,26 @@ public:
 			return 1;
 			// also check for quit - jk, actually handled in calcInteractive!
 		} else if (size == 3) {  // (Trisha)
-			return performOp(tokens[0], tokens[1], tokens[2]); 
-			
-		} else if (size == 5) {  // (Trisha)
-			int result = performOp(tokens[2], tokens[3], token[4]);
-
-			map<string, int>::iterator it = variables.find(tokens[0]);
-                        if (it == variables.end()) {
-			    variables.push_back(result); 
-                        } else {
-			    if (!isVarName(tokens[0])) {
-				return 0; 
-			    }
-			    variables[tokens[0]] = result; 
+			bool success = false; 
+			int value =  performOp(tokens[0], tokens[1], tokens[2], success); 
+			if (!success) {
+			    return 0; 
 			} 
+			result = value; 
+		} else if (size == 5) {  // (Trisha)
+			int success = false; 
+			int value = performOp(tokens[2], tokens[3], token[4], success);
+			if (!success) {
+			    return 0; 
+			}
 			if (tokens[1] != "=") {
 			    return 0; 
 			}
+			if (!isVarName(tokens[0])) {
+			    return 0;
+			}
+			variables[tokens[0]] = value; 
+			result = value; 
 		} else {
 			return 0;
 		}
@@ -67,13 +69,13 @@ public:
 private:
     // private member functions
 	vector<string> tokenize(const string &expr) {
-    	vector<string> vec;
-    	stringstream s(expr);
-    	string tok;
-    	while (s >> tok) {
+	   vector<string> vec;
+	   stringstream s(expr);
+	   string tok;
+	   while (s >> tok) {
         	vec.push_back(tok);
-    	}
-    	return vec;
+	   }
+	   return vec;
 	}
 
 	int isOp (const string &expr) {
@@ -114,32 +116,16 @@ private:
 		return true;
 	}
 
-	int performOp (const string &num1, const string &op, const string &num2) {
+	int performOp (const string &num1, const string &op, const string &num2, bool &success) {
 		int left, right;
-		if (isInt(num1)) {
-		    stringstream ss;
-		    ss << num1;
-		    ss >> left;
-		} else if (isVarName(num1)) {
-		    map<string, int>::iterator it = variables.find(num1);
-                    if (it == variables.end()) {
-			    return 0;
-		    }
-		    left = variables[num1]; 
-		} 
+		
+		left = getNumForOp(num1, success); 
+		if (!success) { return 0; } 
 
-		if (isInt(num2)) {
-                    stringstream ss;
-                    ss << num2;
-                    ss >> right;
-                } else if (isVarName(num2)) {
-                    map<string, int>::iterator it = variables.find(num2);
-                    if (it == variables.end()) {
-                            return 0;
-                    }
-                    right = variables[num2];
-                }
-		    
+		right = getNumForOp(num1, success);
+		if (!success) { return 0; }
+
+		success = true; 
 		switch(c) {
 		    case '+': 
 			return left + right; 
@@ -148,14 +134,37 @@ private:
 		    case '*': 
 			return left * right; 
 		    case '/': 
-			if (right == 0) { /* exit */ }
+			if (right == 0) { 
+			    success = false; 
+			    return 0; 
+			}
 			return left / right; 
 		    default: 
-			//exit 
+			success = false; 
 		}
 
+		return 0; 
 
-	}
+	} 
+
+	int getNumForOp (const string &numStr, bool &success) {
+	    if (isInt(numStr)) {
+                    stringstream ss;
+                    ss << numStr;
+                    ss >> numVal;
+		    success = true; 
+		    return numVal;
+	    if (isVarName(numStr)) {
+		map<string, int>::iterator it = variables.find(numStr);
+                if (it != variables.end()) {
+		    success = true; 
+		    return variables[numStr]; 
+		}
+	    }
+
+	    success = false; 
+	    return 0; 
+	} 
 	
 
 };
